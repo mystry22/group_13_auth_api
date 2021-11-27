65a457287d5f2462f3348e88cb9c66df35a68215
mystry22 <alanemehenry6@gmail.com>
2021-11-27 06:35:25 +0000

    personal data removed

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const testDb = 'mongodb://127.0.0.1/mad';
-const dbUrl = 'mongodb+srv://mystry:mystry.123@lautek.p2i2k.mongodb.net/lautek?retryWrites=true&w=majority';
+const dbUrl = '';
 mongoose.connect(testDb, {useUnifiedTopology:true, useNewUrlParser:true},()=>{
     console.log('db connection to group 13 okay');
 });
